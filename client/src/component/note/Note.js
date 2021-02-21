c7dbb8ba3d8aec7152b7715d95eab1b03fa604f5
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2021-02-21 11:51:23 +0530

    Format date

--- client/src/component/note/Note.js
+++ client/src/component/note/Note.js
@@ -1,26 +1,28 @@
 import "./Note.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import CustomButton from "./../custom-button/CustomButton";
 
-const Note = ({id, title, content, date, deleteNote, updateNote}) => {
-    return (
-        <div className="note">
-            <header>
-                <h1>{title}</h1>
-                <span>{date}</span>
-            </header>
-            <p>{content}</p>
+const Note = ({ id, title, content, date, deleteNote, updateNote }) => {
+  return (
+    <div className="note">
+      <header>
+        <h1>{title}</h1>
+        <span>
+          {new Date(date).toLocaleString(navigator.language, {
+            timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+          })}
+        </span>
+      </header>
+      <p>{content}</p>
 
-            <div className="note-buttons">
-                <CustomButton onClick={() => deleteNote(id)}>Delete</CustomButton>
-                <Link to={`/update/${id}`}>
-                    <CustomButton onClick={() => updateNote(id)}>
-                        edit
-                    </CustomButton>
-                </Link>
-            </div>
-        </div>
-    );
+      <div className="note-buttons">
+        <CustomButton onClick={() => deleteNote(id)}>Delete</CustomButton>
+        <Link to={`/update/${id}`}>
+          <CustomButton onClick={() => updateNote(id)}>edit</CustomButton>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Note;
